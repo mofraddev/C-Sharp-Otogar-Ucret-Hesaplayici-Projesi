b3cc2b964c87c2a869442f2e1d9a1539abcb8337
Barış Zıbahalat Mofrad <mofrad@outlook.com.tr>
2022-03-27 12:05:16 +0300

    Add project files.

---
+++ Nesne Tabanli Programlama OOP Projesi Otogar Hesaplayici/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nesne_Tabanli_Programlama_OOP_Projesi_Otogar_Hesaplayici
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
