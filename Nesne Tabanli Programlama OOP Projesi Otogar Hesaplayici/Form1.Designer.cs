ca5c564b4128ac32411af1fcb5c9dd7f09ea082b
mofraddev <mofrad@outlook.com.tr>
2022-03-29 10:04:54 +0300

    Proje tamamlandı.

--- Nesne Tabanli Programlama OOP Projesi Otogar Hesaplayici/Form1.Designer.cs
+++ Nesne Tabanli Programlama OOP Projesi Otogar Hesaplayici/Form1.Designer.cs
@@ -28,20 +28,232 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Otopark Ödemesi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Araç Tipi: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(63, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Kalış Süresi: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(136, 145);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(283, 103);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Hesapla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(283, 143);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Temizle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox3);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Location = new System.Drawing.Point(66, 214);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(292, 64);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "İNDİRİM KUPONU";
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(195, 31);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(91, 17);
+            this.checkBox3.TabIndex = 0;
+            this.checkBox3.Text = "%75 İNDİRİM";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(98, 31);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(91, 17);
+            this.checkBox2.TabIndex = 0;
+            this.checkBox2.Text = "%50 İNDİRİM";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(7, 31);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(91, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "%25 İNDİRİM";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(66, 314);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "TOPLAM TUTAR:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(66, 359);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "İNDİRİM TUTARI:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(66, 397);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(162, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "ÖDENMESİ GEREKEN TUTAR:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(280, 314);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(280, 359);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(280, 397);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.TabIndex = 6;
+            this.label9.Text = "label9";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(136, 106);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(100, 21);
+            this.comboBox1.TabIndex = 7;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(408, 475);
+            this.ClientSize = new System.Drawing.Size(426, 534);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Soru 2:";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- Nesne Tabanli Programlama OOP Projesi Otogar Hesaplayici/Form1.cs
+++ Nesne Tabanli Programlama OOP Projesi Otogar Hesaplayici/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Nesne_Tabanli_Programlama_OOP_Projesi_Otogar_Hesaplayici
@@ -15,6 +8,103 @@ namespace Nesne_Tabanli_Programlama_OOP_Projesi_Otogar_Hesaplayici
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("Taksi");
+            comboBox1.Items.Add("Minibüs");
+            comboBox1.Items.Add("Otobüs");
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == -1)
+            {
+                MessageBox.Show("Lütfen araç türü seçiniz.");
+                return;
+            }
+            int outval = 0;
+            if (textBox1.Text == null || !(int.TryParse(textBox1.Text, out outval)))
+            {
+                MessageBox.Show("Kalış süresi hatalı girildi.");
+                return;
+            }
+            if (!(checkBox1.Checked) && !(checkBox2.Checked) && !(checkBox3.Checked))
+            {
+                MessageBox.Show("İndirim oranı seçmek zorundasınız.");
+                return;
+            }
+            double toplam_tutar = 0, indirim_tutar = 0, odenmesi_gereken = 0;
+            if (comboBox1.SelectedItem == "Taksi")
+            {
+                toplam_tutar = 2 + (Convert.ToDouble(textBox1.Text) * 5);
+                if (checkBox1.Checked)
+                {
+                    indirim_tutar = toplam_tutar / 100 * 25;
+                    odenmesi_gereken = toplam_tutar - indirim_tutar;
+                }
+                else if (checkBox2.Checked)
+                {
+                    indirim_tutar = toplam_tutar / 100 * 50;
+                    odenmesi_gereken = toplam_tutar - indirim_tutar;
+                }
+                else if (checkBox3.Checked)
+                {
+                    indirim_tutar = toplam_tutar / 100 * 75;
+                    odenmesi_gereken = toplam_tutar - indirim_tutar;
+                }
+            }
+            else if (comboBox1.SelectedItem == "Minibüs")
+            {
+                toplam_tutar = 3 + (Convert.ToDouble(textBox1.Text) * 7);
+                if (checkBox1.Checked)
+                {
+                    indirim_tutar = toplam_tutar / 100 * 25;
+                    odenmesi_gereken = toplam_tutar - indirim_tutar;
+                }
+                else if (checkBox2.Checked)
+                {
+                    indirim_tutar = toplam_tutar / 100 * 50;
+                    odenmesi_gereken = toplam_tutar - indirim_tutar;
+                }
+                else if (checkBox3.Checked)
+                {
+                    indirim_tutar = toplam_tutar / 100 * 75;
+                    odenmesi_gereken = toplam_tutar - indirim_tutar;
+                }
+            } else if (comboBox1.SelectedItem == "Otobüs")
+            {
+                toplam_tutar = 5 + (Convert.ToDouble(textBox1.Text) * 10);
+                if (checkBox1.Checked)
+                {
+                    indirim_tutar = toplam_tutar / 100 * 25;
+                    odenmesi_gereken = toplam_tutar - indirim_tutar;
+                }
+                else if (checkBox2.Checked)
+                {
+                    indirim_tutar = toplam_tutar / 100 * 50;
+                    odenmesi_gereken = toplam_tutar - indirim_tutar;
+                }
+                else if (checkBox3.Checked)
+                {
+                    indirim_tutar = toplam_tutar / 100 * 75;
+                    odenmesi_gereken = toplam_tutar - indirim_tutar;
+                }
+            }
+            label7.Text = toplam_tutar.ToString();
+            label8.Text = indirim_tutar.ToString();
+            label9.Text = odenmesi_gereken.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label7.Text = null;
+            label8.Text = null;
+            label9.Text = null;
+            textBox1.Text = null;
+            comboBox1.SelectedIndex = -1;
         }
     }
 }
